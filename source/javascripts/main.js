bdf7fe9f679f9e5bdcd75aec6039a722b2593f44
Oliver Farrell <oliver@ledgardjepson.com>
2015-02-12 17:21:30 +0000

    WIP

--- source/javascripts/main.js
+++ source/javascripts/main.js
@@ -1,3 +1,37 @@
 Helpers.ready(function() {
-  console.log(Lightbox.template);
+
+  FastClick.attach(document.body);
+
+  yepnope({
+    test : Modernizr.inlinesvg,
+    yep: [
+      './libraries/inline-svg/inline-svg.js'
+    ],
+    callback: function (url, result, key) {
+      if (result) {
+        inlineSVG();
+      } else {
+        var svgs = document.querySelectorAll('img.svg');
+        for (i = 0; i < svgs.length; i++) {
+          var fallback = svgs[i].getAttribute('data-fallback');
+          svgs[i].src = fallback;
+        }
+      }
+    }
+  });
+
+  yepnope({
+    test : Modernizr.input.placeholder,
+    nope: [
+      '/source/components/jquery-placeholder/jquery.placeholder.js'
+    ],
+    callback: function (url, result, key) {
+      if (!result) {
+        document.getElementsByTagName('input, textarea').placeholder();
+      }
+    }
+  });
+
+  picturefill();
+
 });
