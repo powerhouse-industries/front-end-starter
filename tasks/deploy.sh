a61bb477944259c0db19b4052eab09e978396a1d
Oliver Farrell <oliver@ledgardjepson.com>
2015-04-27 11:53:42 +0100

    Fixed some formatting and comments

--- tasks/deploy.sh
+++ tasks/deploy.sh
@@ -8,20 +8,15 @@ mkdir build/javascripts/templates
 mkdir build/javascripts/libraries
 mkdir build/images
 mkdir build/css
-echo "$(tput setaf 2)Done!$(tput sgr 0)"
 
 # Generate Sass
 sh tasks/sass.sh
-echo "$(tput setaf 2)Done!$(tput sgr 0)"
 
 # Generate JavaScript
 sh tasks/javascripts.sh
-echo "$(tput setaf 2)Done!$(tput sgr 0)"
 
 # Optimise images
 sh tasks/images.sh
-echo "$(tput setaf 2)Done!$(tput sgr 0)"
 
 # Check HTML
 sh tasks/html.sh
-echo "$(tput setaf 2)Done!$(tput sgr 0)"
