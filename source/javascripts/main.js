4a36913f1c4222dd6a737824c3b7d9725e2aa619
Oliver Farrell <oliver@ledgardjepson.com>
2015-04-27 15:53:27 +0100

    Removed ready() from around main.js as it's being loading async and is not needed

--- source/javascripts/main.js
+++ source/javascripts/main.js
@@ -1,43 +1,39 @@
-Helpers.ready(function() {
+/**
+ * Attach FastClick to the body
+ */
+FastClick.attach(document.body);
 
-  /**
-   * Attach FastClick to the body
-   */
-  FastClick.attach(document.body);
-
-  /**
-   * Initialise inlineSVG
-   */
-  if (Modernizr.inlinesvg) {
-    Helpers.loadJS('/javascripts/libraries/inlineSVG.js', function() {
-      inlineSVG();
-    });
-  } else {
-    var svgs = document.querySelectorAll('img.svg');
-    for (i = 0; i < svgs.length; i++) {
-      var fallback = svgs[i].getAttribute('data-fallback');
-      svgs[i].src = fallback;
-    }
-  }
-
-  /**
-   * Initialise placeholders for browsers
-   * that don't support them.
-   */
-  if (!Modernizr.input.placeholder) {
-    Helpers.loadJS('/javascripts/libraries/placeholders.min.js');
-  }
-
-  /**
-   * Initialise Cookie Disclaimer
-   */
-  CookieDisclaimer.init({
-    template: '/javascripts/templates/cookie-banner.html'
+/**
+ * Initialise inlineSVG
+ */
+if (Modernizr.inlinesvg) {
+  Helpers.loadJS('/javascripts/libraries/inlineSVG.js', function() {
+    inlineSVG();
   });
+} else {
+  var svgs = document.querySelectorAll('img.svg');
+  for (i = 0; i < svgs.length; i++) {
+    var fallback = svgs[i].getAttribute('data-fallback');
+    svgs[i].src = fallback;
+  }
+}
 
-  /**
-   * Initialise Picturefill
-   */
-  picturefill();
+/**
+ * Initialise placeholders for browsers
+ * that don't support them.
+ */
+if (!Modernizr.input.placeholder) {
+  Helpers.loadJS('/javascripts/libraries/placeholders.min.js');
+}
 
+/**
+ * Initialise Cookie Disclaimer
+ */
+CookieDisclaimer.init({
+  template: '/javascripts/templates/cookie-banner.html'
 });
+
+/**
+ * Initialise Picturefill
+ */
+picturefill();
