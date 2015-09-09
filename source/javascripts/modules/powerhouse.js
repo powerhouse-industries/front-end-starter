985d2d0e2642daee6bfaee93d3ccbad0cecc31e5
Oliver Farrell <oliver@oliverjfarrell.co.uk>
2015-09-09 09:54:21 +0100

    Updates to NPM packages. Updates to Bower packages. Fixed formatting issues with PowerHouse.

--- source/javascripts/modules/powerhouse.js
+++ source/javascripts/modules/powerhouse.js
@@ -4,8 +4,7 @@ var PowerHouse = (function () {
    * A vanilla JavaScript alternative to jQuery's $(document).ready();
    *
    * @public
-   * @param {function} callback - The function to be executed when the document is ready
-   * @returns {function}
+   * @param {Function} callback - The function to be executed when the document is ready
    */
   var ready = function (callback) {
 
@@ -27,9 +26,9 @@ var PowerHouse = (function () {
    * A vanilla JavaScript alternative to jQuery's $(document).on();
    *
    * @public
-   * @param {object} el - The element being targetted
-   * @param {string} eventName - The name of the event
-   * @param {function} callback - The callback
+   * @param {Object} el - The element being targetted
+   * @param {String} eventName - The name of the event
+   * @param {Function} callback - The callback
    */
   var addEventListener = function (el, eventName, callback) {
 
@@ -47,7 +46,7 @@ var PowerHouse = (function () {
    * A vanilla JS alternative to $().next()
    *
    * @public
-   * @param {object} el - The element being targetted
+   * @param {Object} el - The element being targetted
    */
   var nextElementSibling = function(el) {
 
@@ -60,8 +59,8 @@ var PowerHouse = (function () {
    * A vanilla JS alternative to $().each();
    *
    * @public
-   * @param {string} el - The element being targetted
-   * @param {function} callback - The callback
+   * @param {String} selector - The element being targetted
+   * @param {Function} callback - The callback
    */
   var forEachElement = function (selector, callback) {
 
@@ -76,8 +75,8 @@ var PowerHouse = (function () {
    * A vanilla JS alternative to $().hasClass();
    *
    * @public
-   * @param {object} el - The element being targetted
-   * @param {string} className - The class name to query
+   * @param {Object} el - The element being targetted
+   * @param {String} className - The class name to query
    */
   var hasClass = function (el, className) {
 
@@ -89,9 +88,8 @@ var PowerHouse = (function () {
    * Get the contents of a file on the same domain
    *
    * @public
-   * @param {string} file - The url of the file
-   * @param {function} callback - The name of the event
-   * @returns {string}
+   * @param {String} file - The url of the file
+   * @param {Function} success - The name of the event
    */
   var getFileContents = function (file, success, error) {
 
@@ -134,8 +132,8 @@ var PowerHouse = (function () {
    * A vanilla JS alternative to $().insertAfter();
    *
    * @public
-   * @param {string} newNode - The HTML to insert
-   * @param {function} referenceNode - The node to insert after
+   * @param {String} newNode - The HTML to insert
+   * @param {Function} referenceNode - The node to insert after
    */
   var insertAfter = function (newNode, referenceNode) {
 
@@ -146,35 +144,39 @@ var PowerHouse = (function () {
   /**
    * Get the positon of an element on the page
    *
-   * 1. Get the enclosing rectangle.
-   * 2. Calculate the page scroll. All browsers except IE<9 support `pageXOffset/
-   *    pageYOffset`, and in IE when DOCTYPE is set, the scroll can be taken from
-   *    documentElement(<html>), otherwise from `body` - so we take what we can.
-   * 3. The document (`html` or `body`) can be shifted from left-upper corner in
-   *    IE. Get the shift.
-   * 4. Add scrolls to window-relative coordinates and substract the shift of
-   *    `html/body` to get coordinates in the whole document.
-   *
    * @public
-   * @param {object} el - The element being targetted
+   * @param {Object} el - The element being targetted
    */
 
   var getOffsetRect = function (el) {
-    /* 1 */
+
+    /**
+     * 1. Get the enclosing rectangle
+     */
     var box = el.getBoundingClientRect();
 
     var body = document.body;
     var docElement = document.documentElement;
 
-    /* 2 */
+    /**
+     * 2. Calculate the page scroll. All browsers except IE<9 support `pageXOffset/
+     *    pageYOffset`, and in IE when DOCTYPE is set, the scroll can be taken from
+     *    documentElement(<html>), otherwise from `body` - so we take what we can.
+     */
     var scrollTop = window.pageYOffset || docElement.scrollTop || body.scrollTop;
     var scrollLeft = window.pageXOffset || docElement.scrollLeft || body.scrollLeft;
 
-    /* 3 */
+    /**
+     * 3. The document (`html` or `body`) can be shifted from left-upper corner in
+     *     IE. Get the shift.
+     */
     var clientTop = docElement.clientTop || body.clientTop || 0;
     var clientLeft = docElement.clientLeft || body.clientLeft || 0;
 
-    /* 4 */
+    /**
+     * 4. Add scrolls to window-relative coordinates and substract the shift of
+     *    `html/body` to get coordinates in the whole document.
+     */
     var top  = box.top +  scrollTop - clientTop;
     var left = box.left + scrollLeft - clientLeft;
 
@@ -182,15 +184,16 @@ var PowerHouse = (function () {
       top: Math.round(top),
       left: Math.round(left)
     };
+
   };
 
   return {
-    ready : ready,
-    addEventListener : addEventListener,
-    forEachElement : forEachElement,
-    hasClass : hasClass,
-    nextElementSibling : nextElementSibling,
-    getFileContents : getFileContents,
+    ready: ready,
+    addEventListener: addEventListener,
+    forEachElement: forEachElement,
+    hasClass: hasClass,
+    nextElementSibling: nextElementSibling,
+    getFileContents: getFileContents,
     insertAfter: insertAfter,
     getOffsetRect: getOffsetRect
   };
