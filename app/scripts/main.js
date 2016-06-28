5c471043813cb75c840ea3387b3aca16a21f7d53
Oliver Farrell <oliver@oliverjfarrell.co.uk>
2016-06-28 07:57:32 +0100

    WIP

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,9 +1,12 @@
 const attachFastClick = require('fastclick'),
       picturefill     = require('picturefill');
 
-(function() {
+(function() => {
 
   'use strict';
+
+  // Remove the `no-js` class from the `<html>` element
+  document.documentElement.classList.remove('no-js');
 
   // Remove the 300ms delay on some mobile devices
   attachFastClick.attach(document.body);
